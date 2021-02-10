1a86230c8e09b105a5c776a5856b75fa8ee3b22d
mahmoud230 <mahmoudbadwy230@gmail.com>
2021-02-10 17:25:16 +0200

    Update server.config.js

--- config/server.config.js
+++ config/server.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     port: 2020,
-    // mongoUrl : 'mongodb://localhost:27017/cilents',
-    mongoUrl : 'mongodb://192.168.1.8:27017/cilents',
+    mongoUrl : 'mongodb://localhost:27017/cilents',
     mongoConfig: {
-        
     }
 };
